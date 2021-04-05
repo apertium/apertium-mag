b4bee9fbc2d856db45b74c28695e31b192c1445d
William Ball <wball1@swarthmore.edu>
2021-04-04 20:22:46 -0400

    added script

---
+++ tests/disambiguation-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CORPUS=../../ling073-mag-corpus/mag.corpus.basic.txt
+MORPH=../mag.automorf.bin
+RLX=../mag.rlx.bin # you might need to remove ".bin"
+TOKENS=`cat ${CORPUS} | lt-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | wc -l`
+ANALYSES=`cat ${CORPUS} | lt-proc ${MORPH} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
+DISAMB=`cat ${CORPUS} | lt-proc ${MORPH} | cg-proc ${RLX} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
+AMBIGPRE=`calc $ANALYSES/$TOKENS`
+AMBIGPOST=`calc $DISAMB/$TOKENS`
+echo "Ambiguity before disambiguation: ${AMBIGPRE}"
+echo "Ambiguity after disambiguation: ${AMBIGPOST}"
